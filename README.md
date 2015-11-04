be7b6132d8d2e65b4dfe5b5bb97bb84cc091898d
Caleb Scholze <caleb.scholze@gmail.com>
2015-11-04 14:54:31 -0600

    Completed quiz, but used handler functions and not event listeners

--- README.md
+++ README.md
@@ -5,24 +5,24 @@ Time to see how well we've taught you event handling in JavaScript.
 ## Setup
 
 COMPLETED 1. There is sample HTML file content below so create an `index.html` for it and place the content in the body tag.
-2. Link in a JavaScript file named `eventHandling.js`.
-3. Create a CSS file and link it to your HTML. You'll be using `element.classList` to manipulate the CSS classes on elements.
+COMPLETED 2. Link in a JavaScript file named `eventHandling.js`.
+COMPLETED 3. Create a CSS file and link it to your HTML. You'll be using `element.classList` to manipulate the CSS classes on elements.
 
 ## Requirements
 
 > **Note:** Output target is the `output-target` element.
 
-1. When any section is clicked the output target text should be "You clicked on the {text of the section} section"
-2. When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
-3. When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
-4. When you type characters into the input field, the output element should mirror the text in the input field.
-5. When you click the "Add color" button, the `guinea-pig` element's text color should change to blue.
-6. When you click the "Hulkify" button, the `guinea-pig` element's font size should become much larger.
-7. When you click the "Capture it" button, the `guinea-pig` element should have a border added to it.
-8. When you click the "Rounded" button, the `guinea-pig` element's border should become rounded.
-9. The first section's text should be bold.
-10. The last section's text should be bold and italicized.
-11. Make the buttons stop appearing next to each other as inline elements. Change them into block elements.
+COMPLETED 1. When any section is clicked the output target text should be "You clicked on the {text of the section} section"
+COMPLETED 2. When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
+COMPLETED 3. When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
+COMPLETED 4. When you type characters into the input field, the output element should mirror the text in the input field.
+COMPLETED 5. When you click the "Add color" button, the `guinea-pig` element's text color should change to blue.
+COMPLETED 6. When you click the "Hulkify" button, the `guinea-pig` element's font size should become much larger.
+COMPELTED 7. When you click the "Capture it" button, the `guinea-pig` element should have a border added to it.
+COMPLETD 8. When you click the "Rounded" button, the `guinea-pig` element's border should become rounded.
+COMPLETED 9. The first section's text should be bold.
+COMPLETED 10. The last section's text should be bold and italicized.
+COMPLETED 11. Make the buttons stop appearing next to each other as inline elements. Change them into block elements.
 
 #### Boilerplate DOM
 
--- eventHandling.js
+++ eventHandling.js
@@ -1,18 +1,52 @@
-// GET SECTION ELEMENTS
-var sectionElements = document.getElementsByClassName("article-section");
-
 // GET OUTPUT TARGET
-var outputTarget = document.getElementById("output-target");
-outputTarget.innerHTML = "Test Output";
-console.log(outputTarget.innerHTML);
+var outputTargetElement = document.getElementById("output-target");
+
+// GET GUINEA PIG ELEMENT
+var guineaPigElement = document.getElementById("guinea-pig");
 
 
-// ASSIGN EVENT LISTENER AND HANDLER
-for (var sectionElement = 0; sectionElement < sectionElements.length; sectionElement++) {
-		console.log(sectionElements[sectionElement])
-		console.log(sectionElements.innrHTML);
-	// sectionElements[sectionElement].onclick( function (event) {
-	// console.log("You clicked a section");
+// 1. When any section is clicked the output target text should be "You clicked on the {text of the section} section"
+var sectionElements = document.getElementsByClassName("article-section");
+for (var i = 0; i < sectionElements.length; i++) {
+	sectionElements[i].onclick = function(event) {
+		outputTargetElement.innerHTML = "You clicked on the "+ event.target.innerHTML+" section";
+	}
 }
 
-document.getElementsByType
+// 2. When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
+var h1Element = document.getElementById("page-title");
+h1Element.onmouseover = function(event) {
+	outputTargetElement.innerHTML = "You moved your mouse over the header.";
+}
+
+// 3. When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
+h1Element.onmouseleave = function(event) {
+	outputTargetElement.innerHTML = "You left me!!";
+}
+
+// 4. When you type characters into the input field, the output element should mirror the text in the input field.
+var userInput = document.getElementById("keypress-input");
+userInput.onkeyup = function(event) {
+	outputTargetElement.innerHTML = userInput.value;
+}
+
+// 5. When you click the "Add color" button, the `guinea-pig` element's text color should change to blue.
+var addColor = document.getElementById("add-color");
+addColor.onclick = function(event) {
+	guineaPigElement.style.color = "blue";
+}
+// 6. When you click the "Hulkify" button, the `guinea-pig` element's font size should become much larger.
+var makeLarge = document.getElementById("make-large");
+makeLarge.onclick = function(event) {
+	guineaPigElement.style.fontSize = "3em";
+}
+// 7. When you click the "Capture it" button, the `guinea-pig` element should have a border added to it.
+var addBorder = document.getElementById("add-border");
+addBorder.onclick = function(event) {
+	guineaPigElement.style.border = "5px solid red";
+}
+// 8. When you click the "Rounded" button, the `guinea-pig` element's border should become rounded.
+var addRounding = document.getElementById("add-rounding");
+addRounding.onclick = function(event) {
+	guineaPigElement.style.borderRadius = "10px";
+}
